3c20ede10e36c5823b4e4b0c5b47a6551239badc
Samantha Gordon Danner <sgordondanner@gmail.com>
2019-06-24 22:50:41 -0400

    committing some changes

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import quandl
 
 app = Flask(__name__)
 
@@ -6,5 +7,22 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/data', methods=['GET','POST'])
+def get_data():
+  print(request.form['stockName'])
+  stockName = request.form['stockName']
+  quandl.ApiConfig.api_key = "Fkz_a6YzFZjmydcKx8tX"
+  data = quandl.get(stockName)
+  return data.to_json()
+
 if __name__ == '__main__':
   app.run(port=33507)
+
+
+# "EIA/PET_RWTC_D"
+
+# heroku container:push web
+# heroku container:release web
+# https://tranquil-peak-51666.herokuapp.com/ > https://sgd-12daymilestone.herokuapp.com/
+# docker build --tag=milestone .
+# docker run -p 5000:5000 -t -e PORT=5000 milestone
